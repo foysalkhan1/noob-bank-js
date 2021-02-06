a3b6fe813c9b78e35f1975923d9bb46b0d583cb1
foysalkhan1 <foysal.hsbn@gmail.com>
2021-02-06 21:57:14 +0600

    javascript added

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var loginBtn = document.getElementById('login');
+// Login Button Event Handler
+const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function(){
-    console.log('teka de vai teka')
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    const trxArea = document.getElementById('trx-area');
+    trxArea.style.display = 'block';
+});
+// Deposit Event Handler
+const depositBtn = document.getElementById('deposit');
+depositBtn.addEventListener('click', function(){
+    const depositAmount = document.getElementById('depositInput').value;
+    const depositNumber = parseFloat(depositAmount);
+    const currentDeposit = document.getElementById('currentDeposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    document.getElementById('currentDeposit').innerText = totalDeposit;
+    document.getElementById('depositInput').value = '';
+    const currentBalance = document.getElementById('currentBal').innerText;
+    const currentBalanceNumber =parseFloat(currentBalance);
+    const totalBalance = depositNumber + currentBalance;
+    document.getElementById('currentBal').innerHTML = totalBalance; 
 })
