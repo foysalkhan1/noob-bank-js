50dda2182e9c68442d288c11b44724d107f7dd67
foysalkhan1 <foysal.hsbn@gmail.com>
2021-02-07 13:35:23 +0600

    added some withdraw code

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ const depositBtn = document.getElementById('deposit');
 depositBtn.addEventListener('click', function(){
     const depositAmount = document.getElementById('depositInput').value;
     const depositNumber = parseFloat(depositAmount);
-    const currentDeposit = document.getElementById('currentDeposit').innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-    document.getElementById('currentDeposit').innerText = totalDeposit;
     document.getElementById('depositInput').value = '';
-    const currentBalance = document.getElementById('currentBal').innerText;
-    const currentBalanceNumber =parseFloat(currentBalance);
-    const totalBalance = depositNumber + currentBalance;
-    document.getElementById('currentBal').innerHTML = totalBalance; 
+    spanTextUpdate('currentDeposit', depositNumber);
+    spanTextUpdate('currentBal', depositNumber);
 })
+function spanTextUpdate(id, depositNumber) {
+    const currentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber =parseFloat(currentBalance);
+    const totalBalance = depositNumber + currentBalanceNumber;
+    document.getElementById(id).innerHTML = totalBalance; 
+}
